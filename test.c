ab961e48d5e90b294116213f06cc541c6ec4a35d
Rahul Arulkumaran <rahuldravid313@gmail.com>
2018-05-01 20:34:08 +0530

    Add files via upload

---
+++ test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+printf("helloworld");
+int a=0;
+while(a!=1)
+{
+printf("hi");
+a++;
+while(a==0)
+{
+printf("here");
+for(int i=0;i<=0;i++)
+{
+printf("idk");
+printf("vte");
+}
+}
+}
+}
