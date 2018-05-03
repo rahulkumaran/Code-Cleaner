ced3bc1c02d2062fb3e224b02ce4d6d1afaa870b
Rahul Arulkumaran <rahuldravid313@gmail.com>
2018-05-03 21:19:23 +0530

    Delete main.py

--- app/main.py
+++
@@ -1,7 +0,0 @@
-from cleaner import *
-
-if(__name__=='__main__'):
-	filename = str(input("Enter the name of file :\n"))
-	exec_filename = str(input("Enter the name of your executable:\n"))
-	count = code_cleaner(filename)
-	code_execute(exec_filename)
